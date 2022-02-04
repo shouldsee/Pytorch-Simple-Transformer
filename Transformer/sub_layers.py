[] fix gradients and allow save and restart

Transformer/sub_layers.py
@@ -33,7 +33,7 @@ class SelfAttention(nn.Module):
         #Apply attention weights to value
         attention_weighted_value = torch.matmul(attention_weights,value) #(n_query,n_key) matmul (n_key || n_query , d_v)
         attention_weighted_value = self.dropout(attention_weighted_value)
-        
+
         return attention_weighted_value
 class MultiHeadAttention(nn.Module):
     def __init__(self,embed_dim,d_k,d_v,num_heads,mask=False,CUDA=False):
@@ -41,14 +41,15 @@ class MultiHeadAttention(nn.Module):
         self.attention_blocks = [
             SelfAttention(embed_dim,d_k,d_v,mask) for _ in range(num_heads)
         ]
+        [setattr(self,'attention_block_%d'%xi,xxx) for xi,xxx in enumerate(self.attention_blocks)] 
         self.norm = LayerNorm(embed_dim)
         self.CUDA = CUDA
         self.device = torch.device('cuda:0' if CUDA else 'cpu')
-    def forward(self,query,key,value,residual_x):                
-        attention_out = torch.tensor([],requires_grad=True).to(self.device)        
+    def forward(self,query,key,value,residual_x):
+        attention_out = torch.tensor([],requires_grad=True).to(self.device)
         for attention in self.attention_blocks:
-            attention_out = torch.cat((attention_out,attention(query,key,value)),dim=2)        
-        add_and_norm = self.norm(attention_out + residual_x)        
+            attention_out = torch.cat((attention_out,attention(query,key,value)),dim=2)
+        add_and_norm = self.norm(attention_out + residual_x)
         return add_and_norm
 
 class LayerNorm(nn.Module):
@@ -60,8 +61,8 @@ class LayerNorm(nn.Module):
         self.eps = eps
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True) 
-        div =  (std + self.eps) + self.shift        
+        std = x.std(-1, keepdim=True)
+        div =  (std + self.eps) + self.shift
         return self.scale * (x - mean) /(div)
 class PositionWiseFeedForward(nn.Module):
     def __init__(self,embed_dim,output_dim):
@@ -75,7 +76,7 @@ class PositionWiseFeedForward(nn.Module):
         x = torch.max(torch.zeros(x.shape),self.l1(x))
         x = self.RELU(x)
         x = self.l2(x)
-        x = self.dropout(x)        
+        x = self.dropout(x)
         x = self.norm(x+residual_x)
         return x
 
@@ -93,19 +94,20 @@ class VocabLogits(nn.Module):
     def __init__(self,embed_dim,logit_dim):
         super(VocabLogits,self).__init__()
         self.linear = nn.Linear(embed_dim,logit_dim)
-    def forward(self,x):        
+    def forward(self,x):
         return F.log_softmax(self.linear(x),dim=-1)
-    
+
 class Embeddings(nn.Module):
     "Taken from Annotated Transformer (HarvardNLP)"
     def __init__(self,vocab_length,embed_dim,CUDA=False):
         super(Embeddings, self).__init__()
         self.lut = nn.Embedding(vocab_length, embed_dim)
-        self.pos_encode = PositionalEncoding(embed_dim,CUDA=CUDA)
+        # self.pos_encode = PositionalEncoding(embed_dim,CUDA=CUDA)
         self.embed_dim = embed_dim
     def forward(self, x):
         embed = (self.lut(x) * math.sqrt(self.embed_dim))
-        return embed+self.pos_encode(embed)
+        return embed
+        # +self.pos_encode(embed)
 
 class PositionalEncoding(nn.Module):
     "Modified From Annotated Transformer (HarvardNLP)"
@@ -122,8 +124,8 @@ class PositionalEncoding(nn.Module):
         if(CUDA==True):
             pe.type(torch.cuda.FloatTensor)
         self.register_buffer('pe', pe)
-        
-    def forward(self, x):                        
-        x = x + Variable(self.pe[:, :x.size(1)], 
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)],
                          requires_grad=False)
         return x

Transformer/transfomer.py
@@ -13,19 +13,22 @@ class Encoder(nn.Module):
         self.transformer_blocks = [
             TransformerBlock(embed_dim,num_heads,mask=False,CUDA=CUDA) for _ in range(num_blocks)
         ]
+        [setattr(self,'transformer_block_%d'%xi,xxx) for xi,xxx in enumerate(self.transformer_blocks)]
         self.positional_encoding = PositionalEncoding(embed_dim)
-    def forward(self, x):        
-        x = self.positional_encoding(x)        
+    def forward(self, x):
+        # x = self.positional_encoding(x)
         for block in self.transformer_blocks:
-            x = block(x,x,x,x)        
+            x = block(x,x,x,x)
         return x
+
 class Decoder(nn.Module):
     def __init__(self,embed_dim,num_heads,num_blocks,vocab_size,CUDA=False):
         super(Decoder, self).__init__()
         self.multi_head_attention = MultiHeadAttention(embed_dim,embed_dim//num_heads,embed_dim//num_heads,num_heads,mask=False,CUDA=CUDA)
-        self.transformer_blocks = [
+        self.transformer_blocks = xx = [
             TransformerBlock(embed_dim,num_heads,mask=False,CUDA=CUDA) for _ in range(num_blocks)
         ]
+        [setattr(self,'transformer_block_%d'%xi,xxx) for xi,xxx in enumerate(self.transformer_blocks)]
         self.vocab_logits = VocabLogits(embed_dim,vocab_size)
     def forward(self, encoder_outs,x):
         for block in self.transformer_blocks:
@@ -47,9 +50,9 @@ class TransformerTranslator(nn.Module):
         self.decoder = Decoder(embed_dim,num_heads,num_blocks,vocab_size,CUDA=CUDA)
         self.encoded = False
         self.device = torch.device('cuda:0' if CUDA else 'cpu')
-    def encode(self,input_sequence):               
+    def encode(self,input_sequence):
         embedding = self.embedding(input_sequence).to(self.device)
-        self.encode_out = self.encoder(embedding)        
+        self.encode_out = self.encoder(embedding)
         self.encoded = True
     def forward(self,output_sequence):
         if(self.encoded==False):
@@ -60,3 +63,23 @@ class TransformerTranslator(nn.Module):
             return self.decoder(self.encode_out,embedding)
 
 
+class TransformerTranslatorFeng(nn.Module):
+    def __init__(self,embed_dim,num_blocks,num_heads,vocab_size,output_vocab_size,CUDA=False):
+        super(TransformerTranslatorFeng,self).__init__()
+        self.embedding = Embeddings(vocab_size,embed_dim,CUDA=CUDA)
+        self.embedding2 = Embeddings(output_vocab_size,embed_dim,CUDA=CUDA)
+        self.encoder = Encoder(embed_dim,num_heads,num_blocks,CUDA=CUDA)
+        self.decoder = Decoder(embed_dim,num_heads,num_blocks,output_vocab_size,CUDA=CUDA)
+        self.encoded = False
+        self.device = torch.device('cuda:0' if CUDA else 'cpu')
+    def encode(self,input_sequence):
+        embedding = self.embedding(input_sequence).to(self.device)
+        self.encode_out = self.encoder(embedding)
+        self.encoded = True
+    def forward(self,output_sequence):
+        if(self.encoded==False):
+            print("ERROR::TransformerTranslator:: MUST ENCODE FIRST.")
+            return output_sequence
+        else:
+            embedding = self.embedding2(output_sequence)
+            return self.decoder(self.encode_out,embedding)

train_toy_data.py
@@ -32,24 +32,24 @@ z = torch.from_numpy(z).type(torch.LongTensor)
 
 
 import matplotlib.pyplot as plt
-def plot_data(x,y,epoch):    
+def plot_data(x,y,epoch):
     plt.cla()
     plt.clf()
-    fig, ax = plt.subplots()    
+    fig, ax = plt.subplots()
     ax.set_title("Epoch:"+str(epoch))
     fig.dpi = 100.0
     plt.xlim([0, seq_size])
     plt.ylim([0, vocab_size])
     plt.plot(x)
     plt.plot(y)
-    plt.savefig(str(epoch)+".png")    
+    plt.savefig(str(epoch)+".png")
 epoch_num = 100
-test = TransformerTranslator(token_size,num_blocks,num_heads,vocab_size)
+test = TransformerTranslator( token_size, num_blocks, num_heads, vocab_size)
 optimizer = torch.optim.Adam(test.parameters(), lr=1e-2)
 criterion = nn.MSELoss()
 import random
 for i in range(epoch_num):
-    outs = []   
+    outs = []
     avg_loss = []
     ix = y.unsqueeze(0)
     test.zero_grad()
@@ -64,7 +64,7 @@ for i in range(epoch_num):
             iy = iy_label_outs_args#y[:interval_jdx]
         else:
             iy = torch.tensor([0],dtype=torch.int64)
-        #Predict next token        
+        #Predict next token
         iy = iy.unsqueeze(0)
         out = test.forward(iy)
 
@@ -74,13 +74,13 @@ for i in range(epoch_num):
 
         if(interval_jdx!=1):
             torch_outs = torch.cat((torch_outs,out),dim=1)
-            iy_label_outs = torch.cat((iy_label_outs,iy_label),dim=0)           
+            iy_label_outs = torch.cat((iy_label_outs,iy_label),dim=0)
         else:
             torch_outs = out
-            iy_label_outs = iy_label        
+            iy_label_outs = iy_label
         iy_label_outs_args = torch.cat((iy_label_outs_args,torch.tensor([torch.argmax(out)],dtype=torch.int64)),dim=0)
-        outs.append(torch.argmax(out.detach()))    
-    #plot_data(y,outs,i)   
+        outs.append(torch.argmax(out.detach()))
+    #plot_data(y,outs,i)
     loss = criterion(iy_label_outs,torch_outs[0])
     loss.backward()
     optimizer.step()
@@ -90,7 +90,7 @@ for i in range(epoch_num):
 
 import random
 for i in range(1):
-    outs = []   
+    outs = []
     avg_loss = []
     ix = y.unsqueeze(0)
     torch_outs = None
@@ -102,9 +102,9 @@ for i in range(1):
             iy = iy_label_outs_args#y[:interval_jdx]
         else:
             iy = torch.tensor([0],dtype=torch.int64)
-        #Predict next token        
+        #Predict next token
         iy = iy.unsqueeze(0)
-        out = test.forward(iy) 
+        out = test.forward(iy)
 
         iy_label_raw = np.zeros((vocab_size))
         iy_label_raw[y[interval_jdx-1]] = 1
@@ -112,11 +112,11 @@ for i in range(1):
 
         if(interval_jdx!=1):
             torch_outs = torch.cat((torch_outs,out),dim=1)
-            iy_label_outs = torch.cat((iy_label_outs,iy_label),dim=0)    
+            iy_label_outs = torch.cat((iy_label_outs,iy_label),dim=0)
         else:
             torch_outs = out
             iy_label_outs = iy_label
-        
+
         iy_label_outs_args = torch.cat((iy_label_outs_args,torch.tensor([torch.argmax(out)],dtype=torch.int64)),dim=0)
         outs.append(torch.argmax(out.detach()))
 

train_translate.py
@@ -1,11 +1,11 @@
 from Dataset.translation_dataset import EnglishToGermanDataset
-from Transformer.transfomer import TransformerTranslator
+from Transformer.transfomer import TransformerTranslator,TransformerTranslatorFeng
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from tqdm import tqdm 
+from tqdm import tqdm
 import os
 
 
@@ -15,18 +15,19 @@ Hyperparameters
 CUDA = False
 PRINT_INTERVAL = 5000
 VALIDATE_AMOUNT = 10
-SAVE_INTERVAL = 1000
+SAVE_INTERVAL = 50
 
 batch_size = 32
 embed_dim = 64
 num_blocks = 2
-num_heads = 1 #Must be factor of token size
+num_heads = 4 #Must be factor of token size
 max_context_length = 1000
 
-num_epochs = 1000
-learning_rate = 1e-2
+num_epochs = 10
+learning_rate = 1e-3
 
 device = torch.device('cuda:0' if CUDA else 'cpu')
+
 
 """
 Dataset
@@ -38,26 +39,58 @@ dataloader_test = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sh
 """
 Model
 """
-vocab_size = dataset.german_vocab_len
+# vocab_size = dataset.german_vocab_len
+# vocab_size = dataset.english_vocab_len
+# print((dataset.english_vocab_len, dataset.german_vocab_len))
+# assert 0
 torch.set_default_tensor_type(torch.cuda.FloatTensor if CUDA else torch.FloatTensor)
-model = TransformerTranslator(embed_dim,num_blocks,num_heads,vocab_size,CUDA=CUDA).to(device)
+model = TransformerTranslatorFeng(embed_dim,num_blocks,num_heads, dataset.english_vocab_len, dataset.german_vocab_len,CUDA=CUDA).to(device)
 
 """
 Loss Function + Optimizer
 """
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-criterion = nn.KLDivLoss()
+def sqrt_sum( output_log , target):
+    # print(output_log.shape)
+    loss = torch.exp(output_log/2.) * target
+    loss = torch.sum(loss,axis=-1)
+    loss = -torch.mean(loss)
+    #(output-target*2)**3)
+    return loss
+criterion = sqrt_sum
 
+optimizer = torch.optim.Adam( model.parameters(), lr=learning_rate)
+criterion = nn.KLDivLoss(reduction='batchmean')
 """
 Load From Checkpoint
 """
-LOAD = -1
+import sys
+if '--auto' in sys.argv:
+    LOAD_GET_AUTO = 1
+else:
+    LOAD_GET_AUTO = 0
+
+import glob
+from pprint import pprint
+os.makedirs('Checkpoints') if not os.path.exists('Checkpoints') else None
+
+if LOAD_GET_AUTO:
+    res = glob.glob('Checkpoints/*pkl')
+    getLoad = lambda x:int(x.replace('Checkpoints/Checkpoint','').split('.')[0])
+    res = sorted(res,key=getLoad)[-1]
+    LOAD= getLoad(res)
+else:
+    LOAD = -1
+
+if '--LOAD' in sys.argv:
+    LOAD = sys.argv[sys.argv.index('--LOAD')+1]
+    LOAD = int(LOAD)
+
 
 if(LOAD!=-1):
-    checkpoint = torch.load(os.path.join("Checkpoints","Checkpoint"+str(LOAD)+".pkl"))
-    test_losses = checkpoint["test_losses"]
+    checkpoint   = torch.load(os.path.join("Checkpoints","Checkpoint"+str(LOAD)+".pkl"))
+    test_losses  = checkpoint["test_losses"]
     train_losses = checkpoint["train_losses"]
-    num_steps = checkpoint["num_steps"]
+    num_steps    = checkpoint["num_steps"]
     model.load_state_dict(checkpoint["model"])
     optimizer.load_state_dict(checkpoint["optimizer"])
 else:
@@ -68,12 +101,13 @@ else:
 Train Loop
 """
 for epoch in range(num_epochs):
-    running_loss = []   
-    running_test_loss = []     
+    running_loss = []
+    running_test_loss = []
     dataset.train()
     """
     TRAIN LOOP
     """
+    # print(model.state_dict().keys())
     for idx,item in enumerate(tqdm(dataloader)):
         """
         ============================================================
@@ -86,41 +120,42 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         ###################
 
-        ###################        
+        ###################
         #Encode English Sentence
         model.encode(item["english"][:,1:-1])
-        ###################        
+        # print(item["german"][:,1:-1][:1])
+        ###################
 
-        ###################        
+        ###################
         #Output German, One Token At A Time
         all_outs = torch.tensor([],requires_grad=True).to(device)
         for i in range(item["german"].shape[1]-1):
             out = model(item["german"][:,:i+1])
             all_outs = torch.cat((all_outs,out),dim=1)
-        ###################        
+        ###################
 
-        ###################        
+        ###################
         #Mask Out Extra Padded Tokens In The End(Optional)
         all_outs = all_outs * item["logit_mask"][:,1:,:]
         item["logits"] = item["logits"] * item["logit_mask"]
         ###################
 
-        ###################        
+        ###################
         #BackProp
         loss = criterion(all_outs,item["logits"][:,1:,:])
         loss.backward()
         optimizer.step()
-        ###################        
+        ###################
 
         running_loss.append(loss.item())
         num_steps +=1
         """
         ============================================================
         """
-        if(num_steps % PRINT_INTERVAL ==0 or idx==len(dataloader)-1):            
+        if(num_steps % PRINT_INTERVAL ==0 or idx==len(dataloader)-1):
             """
             Validation LOOP
-            """                        
+            """
             all_outs.detach().cpu()
             item["logits"].detach().cpu()
             dataset.test()
@@ -130,7 +165,7 @@ for epoch in range(num_epochs):
                     model.encode(item["english"][:,1:-1])
                     all_outs = torch.tensor([],requires_grad=True).to(device)
                     for i in range(item["german"].shape[1]-1):
-                        out = model(item["german"][:,:i+1])                        
+                        out = model(item["german"][:,:i+1])
                         all_outs = torch.cat((all_outs,out),dim=1)
                     all_outs = all_outs * item["logit_mask"][:,1:,:]
                     item["logits"] = item["logits"] * item["logit_mask"]
@@ -141,7 +176,7 @@ for epoch in range(num_epochs):
             avg_test_loss = np.array(running_test_loss).mean()
             test_losses.append(avg_test_loss)
             avg_loss = np.array(running_loss).mean()
-            train_losses.append(avg_loss)        
+            train_losses.append(avg_loss)
             print("LABEL: ",dataset.logit_to_sentence(item["logits"][0]))
             print("===")
             print("PRED: ",dataset.logit_to_sentence(all_outs[0]))
@@ -149,7 +184,8 @@ for epoch in range(num_epochs):
             print(f"TEST LOSS {avg_test_loss} | EPOCH {epoch}")
             print("BACK TO TRAINING:")
             dataset.train()
-        if(num_steps % SAVE_INTERVAL ==0):            
+
+        if(num_steps % SAVE_INTERVAL ==0):
             torch.save({
                 "model":model.state_dict(),
                 "optimizer":optimizer.state_dict(),